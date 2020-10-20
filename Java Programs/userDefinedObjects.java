2ac37f03c2cfea113ec12d21b3a0a3a5f3604cec
99002762 <72461476+99002762@users.noreply.github.com>
2020-10-20 09:03:21 +0530

    Update userDefinedObjects.java

--- Java Programs/userDefinedObjects.java
+++ Java Programs/userDefinedObjects.java
@@ -4,9 +4,9 @@ import java.util.Iterator;
 
 public class userDefinedObjects {
     public static void main(String args[]){   
-     Student s1=new Student(101,"keerthana",23);  
-     Student s2=new Student(102,"vihitha",21);  
-     Student s3=new Student(103,"manasa",25);   
+     Student s1=new Student(101,"Srisai",23);  
+     Student s2=new Student(102,"Chintu",21);  
+     Student s3=new Student(103,"chinnu",25);   
      ArrayList<Student> al=new ArrayList<Student>();  
      al.add(s1);//
      al.add(s2);  
